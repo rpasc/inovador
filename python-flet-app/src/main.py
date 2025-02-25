25c0e88fabbdb1731d32a98852881bc230a25e13
rpasc <rick.pascoal@gmail.com>
2025-02-25 10:09:05 +0000

    this is my second commit

--- python-flet-app/src/main.py
+++ python-flet-app/src/main.py
@@ -2,7 +2,7 @@ import flet as ft
 from views.app_view import create_view
 
 def main(page: ft.Page):
-    page.title = "Hello Flet App"
+    page.title = "I LOVE PYTHON ..... "
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     create_view(page)
 
